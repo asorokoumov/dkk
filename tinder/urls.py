b3215632b9ab994e08271ffb77432e8aa5b55c25
Anton Sorokoumov <sorokoumov.anton@gmail.com>
2018-11-02 14:08:11 +0300

    fix dislike

--- tinder/urls.py
+++ tinder/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^(?P<user>[-\w]+)/$', views.dkk, name='dkk'),
     url(r'^(?P<user>[-\w]+)/thankyou/$', views.thankyou, name='thankyou'),
     url(r'^(?P<user>[-\w]+)/like/(?P<car>[-\w]+)/$', views.like, name='like'),
-    url(r'^(?P<user>[-\w]+)/disike/(?P<car>[-\w]+)/$', views.dislike, name='dislike'),
+    url(r'^(?P<user>[-\w]+)/dislike/(?P<car>[-\w]+)/$', views.dislike, name='dislike'),
 
 ]
